b4497ad2c46f3ef45cd3da07f43c73a1df79154a
DOBROTA George Alexandru <109147120+dobrotageorge@users.noreply.github.com>
2022-09-07 11:34:40 +0300

    Update VendingMachineTest.java

--- VendingMachineTest.java
+++ VendingMachineTest.java
@@ -1,13 +1,15 @@
 package test;
 
+import main.VendingMachine;
+
 import static main.VendingMachine.computeChange;
 
 class VendingMachineTest {
    public static void main(String[] args) {
-        double sum=7;
+        double sum=5;
         double price=2;
-        int[] result=computeChange(sum,price);
-        int[] expectresult={0, 0, 0, 0, 0, 5};
+        int[] result= VendingMachine.computeChange(sum,price);
+        int[] expectresult= {0, 0, 0, 0, 0, 3};
         if(result==expectresult)
             System.out.println("Test ok");
             else System.out.println("Test fail");
